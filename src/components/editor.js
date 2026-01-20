1019bf50ed12a660636eebedd3b20f761fdae689
Lumisia <zh2171@gmail.com>
2026-01-20 12:11:02 +0900

    최최종

--- src/components/editor.js
+++ src/components/editor.js
@@ -45,7 +45,7 @@ export function useEditorSocket() {
 
     ydoc = new Y.Doc()
     provider = new WebsocketProvider(
-      'https://www.innoutfile.kro.kr/ws/', 
+      'https://wws.innoutfile.kro.kr/ws/', 
       roomName,
       ydoc,
     )
